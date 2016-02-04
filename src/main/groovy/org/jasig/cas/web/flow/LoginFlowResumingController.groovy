af31cb878232f7dc2ec608aa75544e77e60f40b9
Alex Olson <alexkellenolson@gmail.com>
2016-02-04 01:08:36 -0700

    add produces requestmapping annotation

--- src/main/groovy/org/jasig/cas/web/flow/LoginFlowResumingController.groovy
+++ src/main/groovy/org/jasig/cas/web/flow/LoginFlowResumingController.groovy
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpSession
 class LoginFlowResumingController {
     private static final String IDP_AUTHN_FINISHED_EVENT_ID_URL_PARAM = "&_eventId=idpAuthnFinished";
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = "text/html; charset=UTF-8")
     public void resumeLoginFlow(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         /*
         request.setAttribute(FlowExecutionUrlSavingAction.BASE_FLOW_EXECUTION_KEY, session.getAttribute(FlowExecutionUrlSavingAction.BASE_FLOW_EXECUTION_KEY))
